f04344f93b883c0b76dfd5345956baafed839c0d
Violet Hair <violethair2308@gmail.com>
2020-08-23 18:23:17 +0700

    Update EmpowService.js

--- services/EmpowService.js
+++ services/EmpowService.js
@@ -5,20 +5,20 @@ const ping = require("ping")
 const EmpowService = {
     rpc: null,
     async init() {
-        var ips = process.env.RPC_URL.split(",")
+        // var ips = process.env.RPC_URL.split(",")
 
-        var minTime = 99999999
-        var selectHost;
-        for(let i = 0; i < ips.length; i++) {
-            const result = await ping.promise.probe(ips[i])
+        // var minTime = 99999999
+        // var selectHost;
+        // for(let i = 0; i < ips.length; i++) {
+        //     const result = await ping.promise.probe(ips[i])
     
-            if(result.time < minTime) {
-                selectHost = ips[i]
-                minTime = result.time
-            }
-        }
+        //     if(result.time < minTime) {
+        //         selectHost = ips[i]
+        //         minTime = result.time
+        //     }
+        // }
        
-        this.rpc = new empowjs.RPC(new empowjs.HTTPProvider(`http://${selectHost}:30001`))
+        this.rpc = new empowjs.RPC(new empowjs.HTTPProvider(`https://node.empow.io`))
     }
 }
 
